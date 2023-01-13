03253a2cf8f83a887e306d480b93361cd4fd2851
edwin <ackemaneva@126.com>
2023-01-14 03:34:25 +0800

    feat：添加更新时间；repo

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -12,7 +12,7 @@ module.exports = {
   themeConfig: {
     // 网站logo
     logo: "/assets/img/logo.png",
-    lastUpdated: '更新时间', // string | boolean
+    lastUpdated: "更新时间", // string | boolean
     // 导航栏
     nav: [
       { text: "首页", link: "/" },
@@ -32,15 +32,15 @@ module.exports = {
         ],
       },
       { text: "关于我", link: "/about" },
-      { text: "博客源码", link: "https://github.com/perchecc/blog.git" },
-      {
-        text: "Languages",
-        ariaLabel: "Language Menu",
-        items: [
-          { text: "Chinese", link: "/" },
-          { text: "Japanese", link: "/about" },
-        ],
-      },
+      // { text: "博客源码", link: "https://github.com/perchecc/blog.git" },
+      // {
+      //   text: "Languages",
+      //   ariaLabel: "Language Menu",
+      //   items: [
+      //     { text: "Chinese", link: "/" },
+      //     { text: "Japanese", link: "/about" },
+      //   ],
+      // },
     ],
     // 侧边栏
     // sidebar: [
@@ -67,5 +67,23 @@ module.exports = {
       // fallback
       "/": ["" /* / */, "about" /* /about.html */],
     },
+    // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
+    repo: "https://github.com/perchecc/blog",
+    // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
+    // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
+    repoLabel: "查看源码",
+
+    // 以下为可选的编辑链接选项
+
+    // 假如你的文档仓库和项目本身不在一个仓库：
+    // docsRepo: "vuejs/vuepress",
+    // 假如文档不是放在仓库的根目录下：
+    docsDir: "docs",
+    // 假如文档放在一个特定的分支下：
+    docsBranch: "main",
+    // 默认是 false, 设置为 true 来启用
+    editLinks: true,
+    // 默认为 "Edit this page"
+    editLinkText: "帮助我们改善此页面！",
   },
 };
