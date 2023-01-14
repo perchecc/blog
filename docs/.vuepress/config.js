fb73c9d131db45a3777ebc45b7f056dc4e299e9f
edwin <ackemaneva@126.com>
2023-01-14 14:23:38 +0800

    feat：添加pwa

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,90 +1,102 @@
 module.exports = {
-  title: "桃子pêche",
-  description: "桃子的博客，记录前端成长路",
-  head: [
-    ["link", { rel: "icon", href: "/assets/img/logo.png" }],
-    [
-      "meta",
-      { name: "keywords", content: "桃子，博客，前端，成长，记录，日志" },
+    title: "桃子pêche",
+    description: "桃子的博客，记录前端成长路",
+    head: [
+        ["link", { rel: "icon", href: "/assets/img/logo.png" }],
+        ["meta", { name: "keywords", content: "桃子，博客，前端，成长，记录，日志" }],
+        ["meta", { name: "author", content: "桃子" }],
+        // pwa需要
+        ["link", { rel: "manifest", href: "/manifest.json" }],
+        ["meta", { name: "mobile-web-app-capable", content: "yes" }],
+        ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+        ["meta", { name: "application-name", content: "桃子pêche" }],
+        ["meta", { name: "apple-mobile-web-app-title", content: "桃子pêche" }],
+        ["meta", { name: "msapplication-starturl", content: "/" }],
+        ["meta", { name: "apple-mobile-web-app-title", content: "桃子pêche" }],
+        ["meta", { name: "viewport", content: "width=device-width, initial-scale=1, shrink-to-fit=no" }],
+        ["link", { rel: "apple-touch-icon", href: "/assets/img/logo.png" }],
+        ["meta", { name: "theme-color", content: "#3eaf7c" }],
+        ["meta", { name: "apple-mobile-web-app-status-bar-style", content: "black" }],
+        // ["link", { rel: "mask-icon", href: "/icons/safari-pinned-tab.svg", color: "#3eaf7c" }],
+        ["meta", { name: "msapplication-TileImage", content: "/assets/img/logo.png" }],
+        ["meta", { name: "msapplication-TileColor", content: "#000000" }],
     ],
-    ["meta", { name: "author", content: "桃子" }],
-  ],
-  // base: "/blog/",
-  themeConfig: {
-    // 网站logo
-    logo: "/assets/img/logo.png",
-    lastUpdated: "更新时间", // string | boolean
-    // 导航栏
-    nav: [
-      { text: "首页", link: "/" },
-      {
-        text: "前端",
-        link: "/fontend/",
-        items: [
-          {
-            text: "Vue",
-            link: "/fontend/vue/",
-            items: [
-              { text: "vue-router", link: "/fontend/vue/vue-router/" },
-              { text: "vuex", link: "/fontend/vue/vuex/" },
-            ],
-          },
-          { text: "NodeJS", link: "/fontend/node/" },
+    plugins: [
+        "@vuepress/pwa",
+        {
+            serviceWorker: true,
+            updatePopup: true,
+            popupComponent: "MySWUpdatePopup",
+        },
+    ],
+    // base: "/blog/",
+    themeConfig: {
+        // 网站logo
+        logo: "/assets/img/logo.png",
+        lastUpdated: "更新时间", // string | boolean
+        // 导航栏
+        nav: [
+            { text: "首页", link: "/" },
+            {
+                text: "前端",
+                link: "/fontend/",
+                items: [
+                    {
+                        text: "Vue",
+                        link: "/fontend/vue/",
+                        items: [
+                            { text: "vue-router", link: "/fontend/vue/vue-router/" },
+                            { text: "vuex", link: "/fontend/vue/vuex/" },
+                        ],
+                    },
+                    { text: "NodeJS", link: "/fontend/node/" },
+                ],
+            },
+            { text: "关于我", link: "/about" },
+            // { text: "博客源码", link: "https://github.com/perchecc/blog.git" },
+            // {
+            //   text: "Languages",
+            //   ariaLabel: "Language Menu",
+            //   items: [
+            //     { text: "Chinese", link: "/" },
+            //     { text: "Japanese", link: "/about" },
+            //   ],
+            // },
         ],
-      },
-      { text: "关于我", link: "/about" },
-      // { text: "博客源码", link: "https://github.com/perchecc/blog.git" },
-      // {
-      //   text: "Languages",
-      //   ariaLabel: "Language Menu",
-      //   items: [
-      //     { text: "Chinese", link: "/" },
-      //     { text: "Japanese", link: "/about" },
-      //   ],
-      // },
-    ],
-    // 侧边栏
-    // sidebar: [
-    //   {
-    //     title: "前端", // 必要的
-    //     path: "/fontend/vue/", // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-    //     collapsable: false, // 可选的, 默认值是 true,
-    //     sidebarDepth: 1, // 可选的, 默认值是 1
-    //     children: ["/fontend/vue/vuex/", "/fontend/vue/vue-router/"],
-    //   },
-    // ],
-    sidebar: {
-      "/fontend/vue/vue-router/": [
-        "" /* /foo/ */,
-        "one" /* /foo/one.html */,
-        "two" /* /foo/two.html */,
-      ],
-      "/fontend/vue/vuex/": [
-        "" /* /bar/ */,
-        "one" /* /bar/three.html */,
-        "two" /* /bar/four.html */,
-      ],
+        // 侧边栏
+        // sidebar: [
+        //   {
+        //     title: "前端", // 必要的
+        //     path: "/fontend/vue/", // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+        //     collapsable: false, // 可选的, 默认值是 true,
+        //     sidebarDepth: 1, // 可选的, 默认值是 1
+        //     children: ["/fontend/vue/vuex/", "/fontend/vue/vue-router/"],
+        //   },
+        // ],
+        sidebar: {
+            "/fontend/vue/vue-router/": ["" /* /foo/ */, "one" /* /foo/one.html */, "two" /* /foo/two.html */],
+            "/fontend/vue/vuex/": ["" /* /bar/ */, "one" /* /bar/three.html */, "two" /* /bar/four.html */],
 
-      // fallback
-      "/": ["" /* / */, "about" /* /about.html */],
+            // fallback
+            "/": ["" /* / */, "about" /* /about.html */],
+        },
+        // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
+        repo: "https://github.com/perchecc/blog",
+        // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
+        // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
+        repoLabel: "查看源码",
+
+        // 以下为可选的编辑链接选项
+
+        // 假如你的文档仓库和项目本身不在一个仓库：
+        // docsRepo: "vuejs/vuepress",
+        // 假如文档不是放在仓库的根目录下：
+        docsDir: "docs",
+        // 假如文档放在一个特定的分支下：
+        docsBranch: "main",
+        // 默认是 false, 设置为 true 来启用
+        editLinks: true,
+        // 默认为 "Edit this page"
+        editLinkText: "帮助我们改善此页面！",
     },
-    // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
-    repo: "https://github.com/perchecc/blog",
-    // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
-    // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
-    repoLabel: "查看源码",
-
-    // 以下为可选的编辑链接选项
-
-    // 假如你的文档仓库和项目本身不在一个仓库：
-    // docsRepo: "vuejs/vuepress",
-    // 假如文档不是放在仓库的根目录下：
-    docsDir: "docs",
-    // 假如文档放在一个特定的分支下：
-    docsBranch: "main",
-    // 默认是 false, 设置为 true 来启用
-    editLinks: true,
-    // 默认为 "Edit this page"
-    editLinkText: "帮助我们改善此页面！",
-  },
 };
